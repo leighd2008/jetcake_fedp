b0945411090155374eba02c4a4cc3ea993ebe637
leighd2008 <leighd2008@gmail.com>
2020-01-30 15:17:47 -0500

    added hero pic and hero quote to homepage

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import "./hero.styles.scss";
+
+const Hero = () => {
+  const images = require.context("../../assets", true);
+  let imgsrc = images("./jay-heike-6AwjjrzIgRs-unsplash.jpg");
+
+  return (
+    <div className="hero">
+      <div className="heropic">
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${imgsrc})`
+          }}
+        />
+      </div>
+      <div className="heroinfo">
+        <h1 className="title">What is a HERO?</h1>
+        <span className="subtitle">
+          "A hero is somebody who is selfless, who is generous in spirit, who
+          just tries to give back as much as possible and help people. A hero to
+          me is someone who saves people and who really deeply cares."
+          <br /> Debi Mazar
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
+import Hero from "../../components/hero/hero";
+
 import "./homepage.styles.scss";
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="hero">
-      <div className="heropic"></div>
-      <div className="heroinfo">
-        <h1 className="title">What is a HERO?</h1>
-        <span className="subtitle">how to describe a hero</span>
-      </div>
-    </div>
+    <Hero />
   </div>
 );
 
