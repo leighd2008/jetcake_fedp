3a62bcba06f2a651537d27fb9b6edea105e5b8f9
leighd2008 <leighd2008@gmail.com>
2020-01-30 12:44:57 -0500

    created project with create react app and added files for homepage

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="hero">
+      <div className="heropic"></div>
+      <div className="heroinfo">
+        <h1 className="title">What is a HERO?</h1>
+        <span className="subtitle">how to describe a hero</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
