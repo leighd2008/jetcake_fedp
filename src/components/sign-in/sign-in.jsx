d5102bb0f0482bdb0123dac4708c9b8c6d374d32
leighd2008 <leighd2008@gmail.com>
2020-02-01 09:45:06 -0500

    finished sign in submit

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
+import { auth } from "../../firebase/firebase.utils";
 
 import "./sign-in.scss";
 
@@ -15,10 +16,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = event => {
